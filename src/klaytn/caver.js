00c49695aa55dc0f6bb6236005155898fecca51d
dydfuf <88dydfuf@naver.com>
2022-06-27 19:15:55 +0900

    network address 수정

--- src/klaytn/caver.js
+++ src/klaytn/caver.js
@@ -1,11 +1,12 @@
 import Caver from "caver-js";
 
-const BAOBAB_TESTNET_RPC_URL = "https://api.baobab.klaytn.net:8651/";
+const BAOBAB_TESTNET_RPC_URL =
+  "https://public-node-api.klaytnapi.com/v1/cypress";
 
 const rpcURL = BAOBAB_TESTNET_RPC_URL;
 
 const caver = new Caver(rpcURL);
 
-console.log({caver})
+console.log({ caver });
 
 export default caver;
