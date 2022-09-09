e3dbbbfa13169d2cbab1584d9558e018bccc223d
dydfuf <88dydfuf@naver.com>
2022-09-09 18:23:23 +0900

    mint 구현

--- src/component/PageOne.jsx
+++ src/component/PageOne.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
-import { enableKlaytn, accountTruncate } from "../utils.js";
+import { useEffect, useState } from 'react';
+import { enableKlaytn, accountTruncate, check_status, publicMint } from '../utils.js';
 
 export default function PageOne() {
   const [mintCount, setMintCount] = useState(1);
@@ -28,6 +28,15 @@ export default function PageOne() {
     setIsEnabled(true);
   };
 
+  const handleClickMintClick = () => {
+    if (!accounts.length > 0) {
+      alert('지갑을 연결 해주세요');
+    }
+    if (check_status()) {
+      publicMint({ amoutn: mintCount, account: accounts });
+    }
+  };
+
   return (
     <div className="flex flex-col ">
       <button className="ml-auto w-[224px] h-44 rounded-[6px] border-white border-[1px] mt-68">
@@ -35,15 +44,8 @@ export default function PageOne() {
           {isEnabled ? accountTruncate(accounts) : `Connect Kaikas`}
         </span>
       </button>
-      <p className="text-white mt-10 text-[64px] font-bold mx-auto">
-        Tiqkf Minting
-      </p>
-      <img
-        className="mx-auto mt-76"
-        src={"Tlqkf.gif"}
-        width={507}
-        height={479}
-      />
+      <p className="text-white mt-10 text-[64px] font-bold mx-auto">Tiqkf Minting</p>
+      <img className="mx-auto mt-76" src={'Tlqkf.gif'} width={507} height={479} />
       <div className="flex flex-row mt-34 mx-auto space-x-29">
         <button
           className="flex items-center justify-center w-29 h-29 rounded-[4px] border-[1px] border-[#4F4CEE] bg-white"
@@ -59,9 +61,20 @@ export default function PageOne() {
           <span className="text-[#4F4CEE] text-[26px] mb-6 font-bold">+</span>
         </button>
       </div>
-      <button className="mx-auto w-[224px] h-44 rounded-[6px] border-white border-[1px] mt-40">
-        <span className="text-white">Mint</span>
+      <button
+        className="mx-auto w-[224px] h-44 rounded-[6px] border-white border-[1px] mt-40"
+        onClick={handleClickMintClick}
+        disabled={accounts.length === 0}
+      >
+        <span className="text-white">
+          {accounts.length === 0 ? '지갑을 연결 해주세요.' : 'Mint'}
+        </span>
       </button>
     </div>
   );
 }
+
+const NETWORK_MAP = {
+  1001: 'TEST_NET',
+  8217: 'MAIN_NET',
+};
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { ABI, CONTRACTADDRESS } from './constant';
+
 export const CheckUnlocked = async () => {
   return await window.klaytn._kaikas.isUnlocked();
 };
@@ -8,8 +10,8 @@ export const getAccount = () => {
 
 export const enableKlaytn = async () => {
   let accounts;
-  if (typeof window.klaytn !== "undefined") {
-    const provider = window["klaytn"];
+  if (typeof window.klaytn !== 'undefined') {
+    const provider = window['klaytn'];
 
     try {
       accounts = await provider.enable();
@@ -26,6 +28,102 @@ export const disableKlaytn = async () => {
 
 export const accountTruncate = (account) => {
   const first = String(account[0]).substring(0, 6);
-  const end = String(account[0].split("").reverse().join("")).substring(0, 4);
-  return first + " ... " + end;
+  const end = String(account[0].split('').reverse().join('')).substring(0, 4);
+  return first + ' ... ' + end;
 };
+
+export async function check_status() {
+  let health;
+  let mintIndexForSale;
+  let mintLimitPerBlock;
+  let mintStartBlockNumber;
+  let maxSaleAmount;
+  let mintPrice;
+
+  const myContract = new caver.klay.Contract(ABI, CONTRACTADDRESS);
+  await myContract.methods
+    .mintingInformation()
+    .call()
+    .then(function (result) {
+      health = true;
+      mintIndexForSale = parseInt(result[1]);
+      mintLimitPerBlock = parseInt(result[2]);
+      mintStartBlockNumber = parseInt(result[4]);
+      maxSaleAmount = parseInt(result[5]);
+      mintPrice = parseInt(result[6]);
+
+      console.log(`Mint count : ${mintIndexForSale - 1} / ${maxSaleAmount}`);
+      console.log(`트랜잭션당 최대 수량: ${mintLimitPerBlock}개`);
+      console.log(`민팅 시작 블록: #${mintStartBlockNumber.toLocaleString()}`);
+      console.log(`민팅 가격: ${caver.utils.fromPeb(mintPrice, 'KLAY')} KLAY`);
+    })
+    .catch(function (error) {
+      health = false;
+      console.log(error);
+    });
+  const blockNumber = await caver.klay.getBlockNumber();
+  console.log('현재 블록: #' + blockNumber);
+
+  return {
+    health,
+    mintIndexForSale,
+    mintLimitPerBlock,
+    mintStartBlockNumber,
+    maxSaleAmount,
+    mintPrice,
+    blockNumber,
+  };
+}
+
+export async function publicMint({ amount, account }) {
+  if (klaytn.networkVersion === 8217) {
+    console.log('메인넷');
+  } else if (klaytn.networkVersion === 1001) {
+    console.log('테스트넷');
+  } else {
+    alert('ERROR: 클레이튼 네트워크로 연결되지 않았습니다!');
+    return;
+  }
+  if (!account) {
+    alert('ERROR: 지갑을 연결해주세요!');
+    return;
+  }
+
+  const myContract = new caver.klay.Contract(ABI, CONTRACTADDRESS);
+  const { health, mintIndexForSale, mintStartBlockNumber, maxSaleAmount, mintPrice, blockNumber } =
+    await check_status();
+
+  if (!health) {
+    alert('ERROR: 컨트랙트와 연결할 수 없습니다.');
+    return;
+  }
+
+  if (maxSaleAmount + 1 <= mintIndexForSale) {
+    alert('모든 물량이 소진되었습니다.');
+    return;
+  } else if (blockNumber <= mintStartBlockNumber) {
+    alert('아직 민팅이 시작되지 않았습니다.');
+    return;
+  }
+  const total_value = BigNumber(amount * mintPrice);
+
+  try {
+    const gasAmount = await myContract.methods.publicMint(amount).estimateGas({
+      from: account,
+      gas: 6000000,
+      value: total_value,
+    });
+    const result = await myContract.methods.publicMint(amount).send({
+      from: account,
+      gas: gasAmount,
+      value: total_value,
+    });
+    if (result != null) {
+      console.log(result);
+      alert('민팅에 성공하였습니다.');
+    }
+  } catch (error) {
+    console.log(error);
+    alert('민팅에 실패하였습니다.');
+  }
+}
